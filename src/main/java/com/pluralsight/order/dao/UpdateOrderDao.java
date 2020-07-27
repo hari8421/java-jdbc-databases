e7214ff58c7d5ee4c93358b234ca4f40588bbd78
Harikrishnan <harikrishnan8421@gmail.com>
2020-07-27 16:41:10 +0530

    Initial commit

--- src/main/java/com/pluralsight/order/dao/UpdateOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/UpdateOrderDao.java
@@ -52,7 +52,7 @@ public class UpdateOrderDao {
     private PreparedStatement createPreparedStatement(Connection con, ParamsDto paramsDto) throws SQLException {
         PreparedStatement ps=con.prepareStatement(query );
         ps.setString(1,paramsDto.getStatus());
-        ps.setLong(1,paramsDto.getOrderId());
+        ps.setLong(2,paramsDto.getOrderId());
         return ps;
     }
 }
